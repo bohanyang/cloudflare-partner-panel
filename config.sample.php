9aadae5db2d08a4d958e8ba4cf67c5eca5b182a7
Bohan Yang <youthdna@live.com>
2017-12-25 21:52:03 +0800

    Fix

--- config.sample.php
+++ config.sample.php
@@ -5,4 +5,4 @@ $root = "/index.php"; // <= PATH_INFO => $root = ""; (Pretty URL)
 $key = "YOUR_API_KEY";
 $cookie_secure = true; // If you are using HTTP, then set this to false.
 // Login page message
-$msg = "Please enter the login credential of your <strong><a href=\"https://www.cloudflare.com/\" target=\"_blank\" rel=\"nofollow\">Cloudflare</a> account</strong>. Nothing will be saved on our server, forever. The connection to this site is encrypted and authenticated using a security configuration which is rated <strong><a href=\"https://www.ssllabs.com/ssltest/analyze.html?d=ray.bohan.co\" target=\"_blank\" rel=\"nofollow\">A+</a></strong> by SSL Labs.";
+$defmsg = "Please enter the login credential of your <strong><a href=\"https://www.cloudflare.com/\" target=\"_blank\" rel=\"nofollow\">Cloudflare</a> account</strong>. Nothing will be saved on our server, forever. The connection to this site is encrypted and authenticated using a security configuration which is rated <strong><a href=\"https://www.ssllabs.com/ssltest/analyze.html?d=ray.bohan.co\" target=\"_blank\" rel=\"nofollow\">A+</a></strong> by SSL Labs.";
