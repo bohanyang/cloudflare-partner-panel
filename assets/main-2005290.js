0d2ac92945d136de8f2764e3df791c4ebda418f5
Bohan Yang <youthdna@live.com>
2020-05-29 22:24:03 +0900

    Fix JS bug (rewrite the fucking rubbish code)

---
+++ assets/main-2005290.js
@@ -0,0 +1,65 @@
+/* Cloudflare CNAME Setup Main JS */
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
+/* Add Record */
+
+var content = $('#dns-content').show();
+var types = [
+  {
+    value: 'CAA',
+    content: false,
+    element: $('#dns-data-caa').hide()
+  },
+  {
+    value: 'SRV',
+    content: false,
+    element: $('#dns-data-srv').hide()
+  },
+  {
+    value: 'MX',
+    content: true,
+    element: $('#dns-mx-priority').hide()
+  }
+];
+
+$('#type').change(function () {
+  var type;
+  var showContent = true;
+  for (var i = 0; i < types.length; i++) {
+    type = types[i];
+    if (this.value === type.value) {
+      type.element.show();
+      showContent = type.content;
+    } else {
+      type.element.hide();
+    }
+  }
+
+  if (showContent) {
+    content.show();
+  } else {
+    content.hide();
+  }
+});
+
+/* Implement "data-selected" feature */
+
+var selects = document.getElementsByTagName("select");
+
+if (selects) {
+  for (var j = 0; j < selects.length; j++) {
+    var selected = selects[j].getAttribute("data-selected");
+    if (!selected) {
+      continue;
+    }
+    var options = selects[j].getElementsByTagName("option");
+    for (var i = 0; i < options.length; i++) {
+      if (options[i].getAttribute("value") === selected) {
+        options[i].setAttribute("selected", "selected");
+        break;
+      }
+    }
+  }
+}
--- index.php
+++ index.php
@@ -48,7 +48,7 @@ if (!isset($_COOKIE['user_key']) || !isset($_COOKIE['cloudflare_email']) || !iss
 }
 if (!isset($_COOKIE['tlo_cached_main'])) {
 	h2push('assets/tlo-c061807.css', 'style');
-	h2push('assets/main-ed98cf1.js', 'script');
+	h2push('assets/main-2005290.js', 'script');
 	h2push('assets/favicon.ico', 'image');
 	setcookie('tlo_cached_main', 1);
 }
@@ -92,7 +92,7 @@ echo _('Cloudflare CNAME/IP Advanced Setup') . ' - ' . $page_title;
 ?></title>
 	<meta name="renderer" content="webkit">
 	<meta http-equiv="Cache-Control" content="no-siteapp"/>
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha256-eSi1q2PG6J7g7ib17yAaWMcrr5GrtohYChqibrV7PBE=" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/css/bootstrap.min.css" integrity="sha256-aAr2Zpq8MZ+YA/D6JtRD3xtrwpEz2IqOS+pWD/7XKIw=" crossorigin="anonymous">
 	<link rel="stylesheet" href="assets/tlo-c061807.css">
 	<link rel="icon" type="image/x-icon" href="assets/favicon.ico">
 </head>
@@ -168,8 +168,8 @@ if (isset($is_debug) && $is_debug) {
 	echo '<footer class="footer"><small><p>Load time: ' . $time . 's </p></footer>';
 }
 ?>
-	<script src="https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.slim.min.js" integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8=" crossorigin="anonymous"></script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha256-E/V4cWE4qvAeO5MOhjtGtqDzPndRO1LBk8lJ/PR7CA4=" crossorigin="anonymous"></script>
-	<script src="assets/main-ed98cf1.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs=" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/js/bootstrap.bundle.min.js" integrity="sha256-Xt8pc4G0CdcRvI0nZ2lRpZ4VHng0EoUDMlGcBSQ9HiQ=" crossorigin="anonymous"></script>
+	<script src="assets/main-2005290.js"></script>
 </body>
 </html>
