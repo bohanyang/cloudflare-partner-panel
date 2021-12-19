bc6e2eeabf0dd6f499112b9cf8995301289142fe
Bohan Yang <youthdna@live.com>
2021-12-19 12:09:06 +0000

    Fix docker env

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-$host_key = $_SERVER['CFP_HOST_KEY'];
-$page_title = $_SERVER['CFP_TITLE'];  // Optional. Should not use HTML special character.
+$host_key = $_ENV['CFP_HOST_KEY'];
+$page_title = $_ENV['CFP_TITLE'];  // Optional. Should not use HTML special character.
 $tlo_path = '/'; // Optional. The installation path for this panel, ending with '/'. Required for HTTP/2 Push.
 $is_debug = false; // Enable debug mode
